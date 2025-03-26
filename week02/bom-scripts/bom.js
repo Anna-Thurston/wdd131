ab075a3e3ba8b8682e36270daf6b0d8f64c9b6cb
annathurston <sfcakester@gmail.com>
2025-03-26 11:34:45 -0500

    added event listeners

--- week02/bom-scripts/bom.js
+++ week02/bom-scripts/bom.js
@@ -6,10 +6,23 @@ const li = document.createElement('li');
 
 const deleteButton = document.createElement('button');
 
-li.textContent = input.value
 
-deleteButton.textContent = '❌'
+// 
 
-li.append(deleteButton);
 
-list.append(li);
+
+// 
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        li.textContent = input.value
+        deleteButton.textContent = '❌'
+        li.append(deleteButton);
+        list.append(li);
+    }
+})
+
+// deleteButton.addEventListener('click', function () {
+//     list.removeChild(li);
+//     input.focus();
+// })
