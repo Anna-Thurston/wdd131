a5aae0e20df218cf88da60f3b0080b790e72c2a6
annathurston <sfcakester@gmail.com>
2025-03-28 09:32:55 -0500

    fixed css file order in media-query.html file

---
+++ scripts/temples.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    const today = new Date();
+    document.getElementById("currentYear").innerText = today.getFullYear();
+    const text = document.lastModified;
+    document.getElementById("lastModified").innerHTML = text;
+   });
