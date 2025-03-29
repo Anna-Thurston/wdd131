e97413dae503cd1e618f5f82f740b3b4eb08253e
annathurston <sfcakester@gmail.com>
2025-03-28 22:06:34 -0500

    added media temple media queries and responsive hamburger nav menu

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,13 @@
+// Element storage and variables
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Event Listeners
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('open');
+	hambutton.classList.toggle('open');
+});
+
 document.addEventListener("DOMContentLoaded", (event) => {
     const today = new Date();
     document.getElementById("currentYear").innerText = today.getFullYear();
